d0b0ac0350fc023dab9040c14ec7492092e3e1fc
Oscar91-3 <rodrigezoscar139@gmail.com>
2024-08-31 01:44:36 +0000

    Javas.com

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let result = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        result.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+  var lista = document.getElementById("list");
+  lista.innerHTML = result.map(dominio => {
+    return `<li>${dominio}</li>`;
+  });
+  console.log(lista.innerHTML);
   //write your code here
   console.log("Hello Rigo from the console!");
 };
